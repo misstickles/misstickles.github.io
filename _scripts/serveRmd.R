573a9a66993c83ecf49035f8fb25269a8a2a1f99
misstickles <jofaircloth@gmail.com>
2021-05-16 13:52:08 +0100

    finished off covid charts; tidied up

--- _scripts/serveRmd.R
+++ _scripts/serveRmd.R
@@ -1,5 +1,8 @@
+# run using `Rscript.exe "./_scripts/serveRmd.R"
+# https://www.rdocumentation.org/packages/servr/versions/0.22/topics/jekyll
 servr::jekyll(
   input = "_R",
   output = "_posts",
-  command = "bundle exec jeykll build"
+  command = "bundle exec jeykll build",
+  serve = TRUE
 )
